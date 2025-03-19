6334c986a890214747eeb4e7e380bda7396bcc7b
lizyweb <147726740+lizyweb@users.noreply.github.com>
2025-03-19 12:04:09 +0530

    Add files via upload

---
+++ js/main-3.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded',  () => {
+    "use strict";
+  
+    // Hero Header carousel
+    // $(".header-carousel").owlCarousel({
+    //     animateOut: 'slideInRight',
+    //     animateIn: 'slideInRight',
+    //     items: 1,
+    //     autoplay: true,
+    //     smartSpeed: 1000,
+    //     dots: false,
+    //     loop: true,
+    //     nav: true,
+    //     navText: [
+    //         '<i class="bi bi-arrow-left"></i>',
+    //         '<i class="bi bi-arrow-right"></i>'
+    //     ]
+    // });
+});
